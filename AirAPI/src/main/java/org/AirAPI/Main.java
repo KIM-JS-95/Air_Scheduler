7fc18094d7d096cbf45e6479d06346bf9f2efe1d
KIM-JS-95 <baugh248730@gmail.com>
2022-11-10 21:39:07 +0900

    123

--- AirAPI/src/main/java/org/AirAPI/Main.java
+++ AirAPI/src/main/java/org/AirAPI/Main.java
@@ -1,7 +1,14 @@
 package org.AirAPI;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(Main.class, args);
     }
 }

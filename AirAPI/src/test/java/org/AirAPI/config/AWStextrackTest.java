fee967f74e49cc8921140d40668d027269b5a227
KIM-JS-95 <baugh248730@gmail.com>
2023-01-29 20:35:17 +0900

    clear

--- AirAPI/src/test/java/org/AirAPI/config/AWStextrackTest.java
+++ AirAPI/src/test/java/org/AirAPI/config/AWStextrackTest.java
@@ -17,50 +17,61 @@ import java.util.Iterator;
 @TestInstance(TestInstance.Lifecycle.PER_METHOD)
 public class AWStextrackTest {
     private final Logger LOGGER = LoggerFactory.getLogger(AWStextrackTest.class);
-
     @Autowired
     public AWStextrack awStextrack;
 
     @Test
     public void docTest() throws IOException {
-        String filePath = "C:\\Users\\JAESEUNG\\IdeaProjects\\Air_Scheduler\\AirAPI\\src\\main\\resources\\static\\img\\sample.jpg";
+        //String filePath = "C:\\Users\\JAESEUNG\\IdeaProjects\\Air_Scheduler\\AirAPI\\src\\main\\resources\\static\\img\\sample.jpg";
+        String filePath = "D:\\Air_Scheduler\\AirAPI\\src\\main\\resources\\static\\img\\sample.jpg";
         FileInputStream fileInputStream = new FileInputStream(filePath);
         TextractClient textractClient = awStextrack.awsceesser();
         Iterator<Block> blockIterator = AWStextrack.analyzeDoc(textractClient, fileInputStream);
         while (blockIterator.hasNext()) {
             Block block = blockIterator.next();
-            if (block.blockType().toString() == "LINE") {
-                // 객체에 리스트로 넣어주고 저장 시키기
-                LOGGER.info(block.geometry().boundingBox().top().toString());
-                LOGGER.info(block.text());
+            float left = block.geometry().boundingBox().left();
+            float top = block.geometry().boundingBox().top();
+
+            if (block.blockType().toString() == "WORD" && top > 0.05 && left < 0.7) {
+                //LOGGER.info(block.text() + " , " + block.geometry().boundingBox().left() + "/" + block.geometry().boundingBox().top());
+
+                if (left <= 0.1) {
+                    LOGGER.info(block.text());
+                } else if (0.1 < left && left <= 0.18) {
+                    LOGGER.info(block.text());
+                } else if (0.18 < left && left <= 0.24) {
+                    LOGGER.info(block.text());
+                } else if (0.24 < left && left <= 0.3) {
+                    LOGGER.info(block.text());
+                } else if (0.3 < left && left <= 0.37) {
+                    LOGGER.info(block.text());
+                } else if (0.37 < left && left <= 0.46) {
+                    LOGGER.info(block.text());
+                } else if (0.46 < left && left <= 0.52) {
+                    LOGGER.info(block.text());
+                } else if (0.52 < left && left <= 0.58) {
+                    LOGGER.info(block.text());
+                } else if (0.58 < left && left <= 0.65) {
+                    LOGGER.info(block.text());
+                }else{
+                    LOGGER.info(null);
+                }
+                /*
+
+                0.1 / 0.18 / 0.24 / 0.3 / 0.37 / 0.46 / 0.52 / 0.58 / 0.65
+                if(0.1<=left && left<0.25){
+                    LOGGER.info(block.text() + " , " + left +"/"+ top);
+                }else{
+                    LOGGER.info(null);
+                }
+               */
             }
         }
+
     }
 }
 
-
 /*
+date <0.1
 
-Block(
-    BlockType=LINE,
-    Confidence=99.84063,
-    Text=Date,
-    Geometry=Geometry(
-            BoundingBox=BoundingBox(Width=0.03465374, Height=0.011191052, Left=0.01116037, Top=0.033650413),
-            Polygon=[
-                Point(X=0.01116037, Y=0.033671685),
-                Point(X=0.04581153, Y=0.033650413),
-                Point(X=0.045814108, Y=0.044820268),
-                Point(X=0.011162853, Y=0.04484147)
-                ]
-     ),
-    Id=14e5b094-716b-4abb-9667-52135ace8ca2,
-    Relationships=[
-    Relationship(
-        Type=CHILD,
-        Ids=[7e453b43-b3f4-4e2e-97de-69798dce14f2])
-    ],
-    EntityTypes=[]
-)
-
-* */
+*/

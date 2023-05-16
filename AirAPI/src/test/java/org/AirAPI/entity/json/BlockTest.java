3b0452801bdc6e03658a8851e27644dc47710c08
KIM-JS-95 <baugh248730@gmail.com>
2023-05-16 22:00:51 +0900

    123

--- AirAPI/src/test/java/org/AirAPI/entity/json/BlockTest.java
+++ AirAPI/src/test/java/org/AirAPI/entity/json/BlockTest.java
@@ -5,17 +5,12 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.AirAPI.entity.Schedule;
-<<<<<<< HEAD
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-=======
 import org.AirAPI.service.ScheduleService;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Schedules;
 import software.amazon.awssdk.services.textract.model.Block;
->>>>>>> 91ee368f515ca5663dd2b74464019a5b370c13d6
 
 import java.io.File;
 import java.io.IOException;
@@ -31,7 +26,6 @@ class BlockTest {
     @DisplayName("dummy Entity- test")
     public void setEntity() throws IOException {
         List<Blocks> block = readJsonFile();
-<<<<<<< HEAD
         HashMap<String, String> map = new HashMap<>();
 
         block.forEach(callback -> {
@@ -64,38 +58,7 @@ class BlockTest {
                     schedule.setCi(id);
                 } else if (index == 5) {
                     String[] units = id.split(" ");
-=======
-        ex2_test(block);
-    }
-
-    public void ex2_test(List<Blocks> list) {
-        List<Schedule> schedules = new ArrayList<>();
-        Schedule schedule = new Schedule();
-
-        for (int i = 0; i < list.size(); i++) {
-            if (list.get(i).getRowIndex() != 1) {
-                Blocks block = list.get(i);
-                int index = block.getColumnIndex();
-                String chileText = block.getChildText();
-                if (index == 1) {
-                    schedule.setDate(chileText);
-                } else if (index == 2) {
-                    schedule.setPairing(chileText);
-                } else if (index == 3) {
-                    schedule.setDc(chileText);
-                } else if (index == 4) {
-                    schedule.setCi(chileText);
-                } else if (index == 5) {
-                    String[] units = chileText.split(" ");
->>>>>>> 91ee368f515ca5663dd2b74464019a5b370c13d6
-                    if (units.length == 1) {
-                        schedule.setActivity(units[0]);
-                    } else {
-                        schedule.setCo(units[0]);
-                        schedule.setActivity(units[1]);
-                    }
                 } else if (index == 6) {
-<<<<<<< HEAD
                     schedule.setCnt_from(id);
                 } else if (index == 7) {
                     schedule.setStd(id);
@@ -107,19 +70,6 @@ class BlockTest {
                     schedule.setAchotel(id);
                 } else if (index == 11) {
                     schedule.setBlk(id);
-=======
-                    schedule.setCnt_from(chileText);
-                } else if (index == 7) {
-                    schedule.setStd(chileText);
-                } else if (index == 8) {
-                    schedule.setCnt_to(chileText);
-                } else if (index == 9) {
-                    schedule.setSta(chileText);
-                } else if (index == 10) {
-                    schedule.setAchotel(chileText);
-                } else if (index == 11) {
-                    schedule.setBlk(chileText);
->>>>>>> 91ee368f515ca5663dd2b74464019a5b370c13d6
                     schedules.add(schedule);
                     schedule = new Schedule();
                 }
@@ -132,7 +82,8 @@ class BlockTest {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.enable(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY);
         objectMapper.enable(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES);
-        File file = new File("D:\\Air_Scheduler\\AirAPI\\src\\main\\resources\\analyzeDocResponse.json");
+        //File file = new File("D:\\Air_Scheduler\\AirAPI\\src\\main\\resources\\analyzeDocResponse.json");
+        File file = new File("C:\\Users\\KIMJAESUNG\\Air_Scheduler\\AirAPI\\src\\main\\resources\\analyzeDocResponse.json");
         try {
             List<Blocks> entities = objectMapper.readValue(file, new TypeReference<List<Blocks>>() {
             });

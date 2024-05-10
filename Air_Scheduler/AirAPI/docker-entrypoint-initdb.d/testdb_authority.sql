6548708df51fd6f5adaf53a6dd35d025488a43f4
KIM-JS-95 <baugh248730@gmail.com>
2024-05-10 09:15:01 +0900

    schedule api v0.2

--- Air_Scheduler/AirAPI/docker-entrypoint-initdb.d/testdb_authority.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO `testdb`.`authority` (`id`, `authority`) VALUES ('1', 'USER');
-INSERT INTO `testdb`.`authority` (`id`, `authority`) VALUES ('2', 'ADMIN');
--- Air_Scheduler/AirAPI/src/main/java/org/air/repository/AuthorityRepository.java
+++
@@ -1,9 +0,0 @@
-package org.air.repository;
-
-import org.air.entity.Authority;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AuthorityRepository extends JpaRepository<Authority, Long> {
-}
